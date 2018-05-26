df18200fb40ca90a9e4e6776ad26afe98e580b69
EC2 Default User <ec2-user@ip-172-31-35-3.us-east-2.compute.internal>
2018-05-26 16:01:19 +0000

    CRUD tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,18 @@
-@extends('layauts.app')
+@extends('layouts.app')
 
 @section('content')
+
+    <h1>タスク一覧</h1>
+
+    @if(count($tasks) > 0)
+    
+        @foreach($tasks as $task)
+            <li>{!! link_to_route('tasks.show',$task->id,['id'=>$task->id]) !!}:{{ $task -> content }}</li>
+        @endforeach
+        
+    @endif
+    
+    {!! link_to_route('tasks.create','新規タスクの作成') !!}
 
 
 @endsection
