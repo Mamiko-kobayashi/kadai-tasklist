9bd0194a39c4ccb526bf635a0d0e3693ff65df2c
EC2 Default User <ec2-user@ip-172-31-35-3.us-east-2.compute.internal>
2018-06-06 15:46:54 +0000

    bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,19 +2,28 @@
 
 @section('content')
 
+<div class="row">
+  <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+      
     <h1>タスク新規作成ページ</h1>
-    
+
     {!! Form::model($task,['route'=>'tasks.store']) !!}
     
+      <div class="form-group">
         {!! Form::label('content','タスク：') !!}
-        {!! Form::text('content') !!}
-        
+        {!! Form::text('content',null,['class'=>'form-control']) !!}
+      </div>
+      
+      <div class="form-group">
         {!! Form::label('status','ステータス：') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::submit('新規作成') !!}
+        {!! Form::text('status',null,['class'=>'form-control']) !!}
+      </div>
+      
+        {!! Form::submit('新規作成',['class'=>'btn btn-primary']) !!}
     
     {!! Form::close() !!}
+  </div>
+</div>
 
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,18 +2,28 @@
 
 @section('content')
 
+<div class="row">
+  <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+
     <h1>id: {{$task->id}}のタスク編集ページ</h1>
-    
+
     {!! Form::model($task,['route'=>['tasks.update',$task->id],'method'=>'put']) !!}
     
+      <div class="form-group">
          {!! Form::label('content','タスク：') !!}
-         {!! Form::text('content') !!}
+         {!! Form::text('content',null,['class'=>'form-control']) !!}
+      </div>
          
+      <div class="form-group">
          {!! Form::label('status','ステータス：') !!}
-         {!! Form::text('status') !!}
-        
-         {!! Form::submit('更新') !!}
+         {!! Form::text('status',null,['class'=>'form-control']) !!}
+      </div>
+      
+         {!! Form::submit('更新',['class'=>'btn btn-primary']) !!}
     
     {!! Form::close() !!}
+    
+  </div>
+</div>
 
 @endsection
